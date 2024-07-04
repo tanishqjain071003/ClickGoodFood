dd0b5cb03de367479a84e25c3154b44de35c9667
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-05 01:30:17 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -35,6 +35,7 @@ const StoreContextProvider = (props) => {
         for (const item in cartItems) {
             if (cartItems[item] > 0) {
                 let itemInfo = food_list.find((product) => product._id === item);
+                if(itemInfo)
                 totalAmount += itemInfo.price * cartItems[item];
             }
         }
