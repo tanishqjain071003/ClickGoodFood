0bf1e3c25feed2042fc1ab6ec55addf638985c38
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-04 06:04:00 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ setShowLogin }) => {
   const logout = () => {
     localStorage.removeItem("token");
     setToken("");
-    navigate('/myorders')
+    navigate('/')
   }
 
   return (
