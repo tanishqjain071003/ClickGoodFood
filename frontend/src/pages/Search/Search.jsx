fade422027fd96c43c2e45b00f71bba2a4eed497
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-05 03:32:15 +0530

    Update Search.jsx

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,1 +1,20 @@
+import React, { useContext } from 'react'
+import './Search.css'
+import FoodItem from '../../components/FoodItem/FoodItem'
+import { StoreContext } from '../../Context/StoreContext'
 
+const Search = () => {
+    const{results,search} = useContext(StoreContext)
+  return (
+    <div className='foodDisplay' id='foodDisplay'>
+        <h2>Dishes matching '{search}'</h2>
+            <div className='foodDisplayList'>
+            {results.map((item)=>{
+                return <FoodItem key={item._id} image={item.image} name={item.name} desc={item.description} price={item.price} id={item._id}/>
+            })}
+            </div>
+    </div>
+  )
+}
+
+export default Search
