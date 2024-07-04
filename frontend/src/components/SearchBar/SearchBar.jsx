b879ea1207dbcefd50108875a7dae79c8372c8cb
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-05 03:35:40 +0530

    Create SearchBar.jsx

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './SearchBar.css'
+import { assets } from '../../assets/assets'
+import axios from 'axios'
+import { StoreContext } from '../../Context/StoreContext'
+
+
+const SearchBar = ({setResults}) => {
+    
+    const {url,search,setSearch,fetchSearch} = useContext(StoreContext)
+
+    return (
+        <div className = 'search'>
+            <input type="text" placeholder='Search' value = {search} onChange={(e)=>{setSearch(e.target.value) 
+                                                                                     fetchSearch(e.target.value)}}/>
+           
+        </div>
+  )
+}
+
+export default SearchBar
