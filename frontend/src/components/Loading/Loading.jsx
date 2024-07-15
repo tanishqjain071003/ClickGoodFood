dc476151e5cd9dda6bcba9b9a7aa7b780a7cd633
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-15 23:39:34 +0530

    Update Loading.jsx

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import './Loading.css'
 
+const Loading = () => {
+  return (
+    <div className='loading'>
+    <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Loading
