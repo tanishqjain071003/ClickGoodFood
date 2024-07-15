bd7aa0848b0e99a98b96b2d8a105752061322b82
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-15 23:26:09 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,10 +5,29 @@ import { StoreContext } from '../../Context/StoreContext'
 
 const FoodDisplay = ({category}) => {
 
-  const {food_list} = useContext(StoreContext);
-
+  const {url} = useContext(StoreContext);
+  const[food_list,setFoodList] = useState([])
+  const[loading,setLoading] = useState(true);
+  const fetchFoodList = async () => {
+    setLoading(true);
+    try {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data)
+        
+    } catch (error) {
+        console.log(error);
+    }finally{
+        setLoading(false);
+    }  
+}
+  useEffect(()=>{
+    fetchFoodList()
+  },[])
+  
+  console.log(food_list);
+  if(loading)return <div>Loading</div>
   return (
-    <div className='foodDisplay' id='foodDisplay'>
+      <div className='foodDisplay' id='foodDisplay'>
       <h2>Top dishes near you</h2>
       <div className='foodDisplayList'>
         {food_list.map((item)=>{
@@ -17,7 +36,7 @@ const FoodDisplay = ({category}) => {
           }
         })}
       </div>
-    </div>
+      </div>
   )
 }
 
